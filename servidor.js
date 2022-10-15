30267e26a4ce5c9d8a9e02a69df1d27132d25b72
KikeFerCO <jorgef08@gmail.com>
2022-10-14 19:45:42 -0500

    Commit inicial

---
+++ servidor.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+const mongoose = require('mongoose');
+const TareaSchema = require('./modelos/Tarea.js');
+
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json);
+app.use(router);
+mongoose.connect('mongodb+srv://KikeFerCO:58caSI9TexA9@misiontic2022.eranpqf.mongodb.net/express?retryWrites=true&w=majority');
+
+app.listen(3000, () => {
+
+    console.log('Escuchando por puerto 3000');
+
+});
+
+router.get('/', (req, res) => {
+    res.send('Inicio de la API 2');
+
+});
+
+router.post('/tarea', (req, res) => {
+
+    let nuevaTarea = new TareaSchema({
+
+        idTarea: req.body.id,
+        nombreTarea: req.body.nombre,
+        detalleTarea: req.body.detalle
+
+    });
+    nuevaTarea.save(function (err, datos) {
+
+        if (err) {
+            console.log(err);
+        }
+        else {
+            res.send('Tarea almacenada correctamente');
+        }
+    });
+});
+
+router.get('/tarea', (req, res) => {
+
+    TareaSchema.find(function (err, datos) {
+
+        if (err) {
+
+            console.log("Error leyendo las tareas");
+
+        }
+        else {
+
+            res.send(datos);
+
+        }
+    });
+
+});
